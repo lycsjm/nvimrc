51682aafeba2389a6ed251b302803f73aab86d68
amikai <as23041248@gmail.com>
2020-09-29 05:34:51 +0800

    Port defx setting to lua

--- lua/mysetting/config/defx.lua
+++ lua/mysetting/config/defx.lua
@@ -50,7 +50,7 @@ M.keymapping = function()
     mapper('n', 'I', [[defx#do_action('toggle_ignored_files')]],
     {noremap = true, expr = true, silent = true})
 
-    mapper('n', '<Space>', [[defx#do_action('toggle_select')]] .. 'j',
+    mapper('n', '<Space>', [[defx#do_action('toggle_select')]],
     {noremap = true, expr = true, silent = true})
 
     mapper('n', '<Esc>', [[defx#do_action('clear_select_all')]],
@@ -78,6 +78,72 @@ M.keymapping = function()
 
     mapper('n', '!', [[defx#do_action('execute_command')]],
     {noremap = true, expr = true, silent = true})
+
+    mapper('n', 'm', [[<cmd>lua require('mysetting.config.defx').menu()<cr>]],
+    {noremap = true})
+end
+
+local reveal = function()
+    local open_cmd
+    if fn.has('mac') then
+        open_cmd = 'open'
+    elseif fn.has('unix') then
+        open_cmd = 'xdg_open'
+    else
+        return
+    end
+
+    target = fn['defx#get_candidate']()
+    local full_path = fn['fnamemodify'](target.word, ':p') -- is directory
+    if not target.is_directory then
+        full_path = fn['fnamemodify'](target.word, ':p:h') -- is file
+    end
+
+    if full_path == nil then
+        return
+    end
+    os.execute(open_cmd .. ' ' .. full_path)
+end
+
+M.menu = function()
+    local msg = "=========================================================\n"..
+                "  (a)dd a childnode\n" ..
+                "  (A)dd multiple childnodes\n" ..
+                "  (d)elete the current node\n" ..
+                "  (m)ove the current node\n" ..
+                "  (r)eveal in Finder the current node\n"
+
+    vim.cmd [[call utils#echo_succes_msg("Defx context menu\n")]]
+    print(msg)
+
+    local action = fn.nr2char(fn.getchar())
+
+    local action_map = {
+        a = {op = 'new_file',
+            pre_hook = function()
+                print( "Enter the dir/file name to be created. Dirs end with a '/'\n")
+            end,
+            post_hook = function() end},
+        A = {op = 'new_multiple_files',
+            pre_hook = function()
+                print("Enter the dir/file name seperated by space. Dirs end with a '/'\n")
+            end,
+            post_hook = function() end},
+        d = {op = 'remove', pre_hook = function() end, post_hook = function() end},
+        m = {op = 'rename', pre_hook = function() end, post_hook = function() end},
+        r = {op = 'reveal', do_thing = reveal}
+    }
+
+    local target = action_map[action]
+    if target == nil then
+        return 
+    elseif target.op == 'reveal' then
+        target.do_thing()
+        return
+    end
+    target.pre_hook()
+    fn['defx#call_action'](target.op)
+    target.post_hook()
 end
 
 M.setting = function()
--- lua/mysetting/plug.lua
+++ lua/mysetting/plug.lua
@@ -218,7 +218,7 @@ return require('packer').startup(function()
         vim.cmd [[autocmd FileType defx lua require('mysetting.config.defx').keymapping()]]
         vim.cmd [[autocmd BufLeave,BufWinLeave  \[defx\]* call defx#call_action('add_session')]]
         vim.cmd [[noremap <silent><F4> <cmd>Defx -session-file='/tmp/defx_session' -buffer-name="defx"<CR>]]
-        vim.cmd [[autocmd FileType defx lua require('mysetting.config.defx')]]
+        vim.cmd [[autocmd FileType defx lua require('mysetting.config.defx').keymapping()]]
     end,
     config = function()
         require('mysetting.config.defx').setting()
